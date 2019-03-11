1853d3af0c0f6dd594c952bd8685fc6b98146a67
gongxf <gongxf@craiditx.com>
2019-03-11 11:22:36 +0800

    管道测试i

--- ch8/pipeline1/main.go
+++ ch8/pipeline1/main.go
@@ -9,11 +9,17 @@ package main
 import "fmt"
 
 //!+
+// 该示例程序由三个goroutine, 两个通道构成
+// Counter goroutine 负责生产数字0, 1, 2..., 并将生产的数字发送到阻塞通道naturals
+// Squarer goroutine 负责从阻塞通道naturals中取出数字, 计算对应平方, 并将计算结果发送到阻塞通道squares
+// Main goroutine 负责从阻塞通道squares中取出计算结果并打印
 func main() {
+	// 新建阻塞通道naturals, squares
 	naturals := make(chan int)
 	squares := make(chan int)
 
 	// Counter
+	// 通过go关键字创建新的goroutine, 并由后台调度执行
 	go func() {
 		for x := 0; ; x++ {
 			naturals <- x
